5e2d2b05b1ea949b02de1d0c37e9ec9f8193d5e8
Johan Satgé <johansatge@users.noreply.github.com>
2026-04-29 21:42:44 +0200

    Fine tune animations

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
 const node = document.querySelector('[js-emoji]')
 const emojis = ['🌵', '🌴', '🌳', '🌲', '🍁', '🍂', '🌿']
 node.innerText = emojis[Math.floor(Math.random() * emojis.length)]
+node.classList.add('emoji--pop')
